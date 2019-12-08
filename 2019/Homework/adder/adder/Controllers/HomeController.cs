682b7448c37ce038c201cb350a7f8f9aad1522e1
Daniil Lee <d.lee@corp.mail.ru>
2019-12-08 23:59:57 +0300

    1.3 Done

---
+++ 2019/Homework/adder/adder/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using adder.Models;
+
+namespace adder.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Microsoft.AspNetCore.Http.IFormCollection collection)
+        {
+            try
+            {
+                ViewData["Source"] = collection["Source"];
+                ViewData["Sensor"] = collection["Sensor"];
+                ViewData["Parameter"] = collection["Parameter"];
+                ViewData["Value"] = collection["Value"];
+
+                return View("Res");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
