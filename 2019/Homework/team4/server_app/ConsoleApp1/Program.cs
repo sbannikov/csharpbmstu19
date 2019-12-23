e9e10f6afccc414bf49db03aa30e524204e0b89f
ForseIKomar <33393253+ForseIKomar@users.noreply.github.com>
2019-12-23 20:55:38 +0300

    Update Program.cs

--- 2019/Homework/team4/server_app/ConsoleApp1/Program.cs
+++ 2019/Homework/team4/server_app/ConsoleApp1/Program.cs
@@ -1,47 +1,54 @@
-﻿using System;
+using System;
 using System.Data; // определяет классы, интерфейсы, делегаты, которые реализуют архитектуру ADO.NET
 using System.Data.SqlClient; // пределяет функциональность провайдера для баз данных MS SQL Server
+using System.Collections.Generic;
 
 public struct Val
 {
-    public string source_uuid, pniv, sensor_uuid, state, 
-    parameter_uuid, code, unit, type, timestamp_start, 
+    public string source_uuid, pniv, sensor_uuid, state,
+    parameter_uuid, code, unit, type, timestamp_start,
     timestamp_end, value, last_changed;
 }
 
-public static List<Val> GetDataFromDB()
+class Roma
 {
-    List<Val> Vals = new List<Val>();
-    string connectionString = "Server=db;Port=5432;Database=postgres;UserId=postgres;Password=postgres;";
-    string queryString = "SELECT * FROM latestvals";
-
-    using (SqlConnection connection = new SqlConnection(connectionString))
+    public static List<Val> GetDataFromDB()
     {
-        connection.Open();       
-        using (SqlCommand command = new SqlCommand(queryString, connection))
+        List<Val> Vals = new List<Val>();
+        string connectionString = "Server=db;Port=5432;Database=postgres;UserId=postgres;Password=postgres;";
+        string queryString = "SELECT * FROM latestvals";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
         {
-            using (SqlDataReader reader = command.ExecuteReader())
+            connection.Open();
+            using (SqlCommand command = new SqlCommand(queryString, connection))
             {
-                List<Val> Vals = new List<Val>();
-                while (reader.Read())
+                using (SqlDataReader reader = command.ExecuteReader())
                 {
-                    var oneValue = new Val();
-                    oneValue.source_uuid        = reader.GetString(0);
-                    oneValue.pniv               = reader.GetString(1);
-                    oneValue.sensor_uuid        = reader.GetString(2);
-                    oneValue.state              = reader.GetString(3);
-                    oneValue.parameter_uuid     = reader.GetString(4);
-                    oneValue.code               = reader.GetString(5);
-                    oneValue.unit               = reader.GetString(6);
-                    oneValue.type               = reader.GetString(7);
-                    oneValue.timestamp_start    = reader.GetString(8);
-                    oneValue.timestamp_end      = reader.GetString(9);
-                    oneValue.value              = reader.GetString(10);
-                    oneValue.last_changed       = reader.GetString(11);
-                    Vals.Add(oneValue);
+                    while (reader.Read())
+                    {
+                        var oneValue = new Val();
+                        oneValue.source_uuid = reader.GetString(0);
+                        oneValue.pniv = reader.GetString(1);
+                        oneValue.sensor_uuid = reader.GetString(2);
+                        oneValue.state = reader.GetString(3);
+                        oneValue.parameter_uuid = reader.GetString(4);
+                        oneValue.code = reader.GetString(5);
+                        oneValue.unit = reader.GetString(6);
+                        oneValue.type = reader.GetString(7);
+                        oneValue.timestamp_start = reader.GetString(8);
+                        oneValue.timestamp_end = reader.GetString(9);
+                        oneValue.value = reader.GetString(10);
+                        oneValue.last_changed = reader.GetString(11);
+                        Vals.Add(oneValue);
+                    }
                 }
             }
-        }                    
+        }
+        return Vals;
     }
-    return Vals;
+    static void Main(string[] args)
+    {
+
+    }
 }
