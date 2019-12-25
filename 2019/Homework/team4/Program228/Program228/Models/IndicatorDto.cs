80f8948e20b4bb0f773e7a45f07ccb885037be60
Roman Vekshin <vekshin.roman@student.bmstu.ru>
2019-12-25 13:08:56 +0300

    Update IndicatorDto.cs

--- 2019/Homework/team4/Program228/Program228/Models/IndicatorDto.cs
+++ 2019/Homework/team4/Program228/Program228/Models/IndicatorDto.cs
@@ -3,9 +3,17 @@ namespace Program228.Models
 {
     public class IndicatorDto
     {
+        public String Source_uuid { get; set; }
+        public String Pniv { get; set; }
+        public String Sensor_uuid { get; set; }
+        public String State { get; set; }
+        public String Parameter_uuid { get; set; }
         public String Code { get; set; }
         public String Unit { get; set; }
         public String Type { get; set; }
+        public String Timestamp_start { get; set; }
+        public String Timestamp_end { get; set; }
         public String Value { get; set; }
+        public String Last_changed { get; set; }
     }
 }
