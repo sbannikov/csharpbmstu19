95d00dddf19bcf72adedd8d5645acf490fd56f1e
eldar1998man <41110381+eldar1998man@users.noreply.github.com>
2019-12-25 19:21:15 +0300

    Addin json getter

---
+++ 2019/Homework/team5/GetterService/GetterService/IDataReceiver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace GetterService
+{
+    [ServiceContract]
+    public interface IDataGetter
+    {
+        [OperationContract]
+        string GetData(String incomingJson);
+    }
+
+}
