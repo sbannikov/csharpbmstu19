8f456730ccd85b51c217ee0ae28d10c7c1c2c886
yury98 <yshashkin@yandex.ru>
2019-12-25 12:32:59 +0300

    homework team 3

---
+++ 2019/Homework/team-3/Monitoring/MetricsDbProvider/Interface/IDbDataProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MetricsDbProvider.Models;
+
+namespace MetricsSave.Interface
+{
+	public interface IDbDataProvider
+	{
+		void AddMark(Mark item);
+
+		IEnumerable<Source> GetSources();
+
+		IEnumerable<Sensor> GetSensors(string sourceId);
+
+		IEnumerable<Metrics> GetMetrics(string sensorId);
+
+		IEnumerable<Mark> GetMarks();
+	}
+}
