33f5a7ab301fc703cabea731646fb46e579af69e
Bannikov Sergey <sbannikov@croc.ru>
2020-05-08 18:21:08 +0300

    Домашнее задание - Чунихин

--- 2019/Homework/homeworks/team_1/adder/adder/Models/SensorModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace adder.Models
-{
-    public class SensorModel
-    {
-        public string SensorID { get; set; }
-        public string State { get; set; }
-    }
-}
--- 2019/Homework/homeworks/team_1/adder/adder/Models/SourceModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace adder.Models
-{
-    public class SourceModel
-    {
-        public string SourceID { get; set; }
-        public string Pniv { get; set; }
-    }
-}
