cf88c691acd283c34bff783e71e60d1af15ef4fa
Bannikov Sergey <sbannikov@croc.ru>
2020-05-08 18:41:56 +0300

    Домашнее задание - Барашкова

---
+++ 2019/Homework/IU6-71/Барашкова/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowDZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.age = new System.Windows.Forms.NumericUpDown();
+            this.name = new System.Windows.Forms.TextBox();
+            this.surname = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.age)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 104);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Фамилия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Возраст";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(118, 267);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Пол";
+            // 
+            // age
+            // 
+            this.age.Location = new System.Drawing.Point(295, 183);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(120, 26);
+            this.age.TabIndex = 5;
+            this.age.ValueChanged += new System.EventHandler(this.age_ValueChanged);
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(295, 32);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(168, 26);
+            this.name.TabIndex = 6;
+            this.name.TextChanged += new System.EventHandler(this.name_TextChanged);
+            // 
+            // surname
+            // 
+            this.surname.Location = new System.Drawing.Point(295, 104);
+            this.surname.Name = "surname";
+            this.surname.Size = new System.Drawing.Size(168, 26);
+            this.surname.TabIndex = 7;
+            this.surname.TextChanged += new System.EventHandler(this.surname_TextChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(368, 267);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 24);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ж";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(295, 267);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 24);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "М";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 450);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.surname);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.age);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "WindowDZ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.age)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown age;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox surname;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ 2019/Homework/IU6-71/Барашкова/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowDZ
+{
+    public partial class Form1 : Form
+    {
+        string named,surnamed;
+        int aged;
+        string gender, conc;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void surname_TextChanged(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void age_ValueChanged(object sender, EventArgs e)
+        {
+            
+    }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql, Output="";
+            SqlCommand command;
+            SqlDataReader dataRead;
+            if ((name.Text == "") || (surname.Text == "") || (age.Value == 0))
+            { MessageBox.Show("Введите все параметры"); }
+            else
+            {
+                named = name.Text;
+                surnamed = surname.Text;
+                aged = Decimal.ToInt16(age.Value);
+                if (radioButton1.Checked)
+                { gender = "Мужской"; }
+                else { gender = "Женский"; }
+                conc = surnamed +", "+ named +", "+ aged.ToString() +", "+ gender;
+                MessageBox.Show(conc, "Запись в БД");
+                string connetionString;
+                SqlConnection cnn;
+                connetionString = @"Data Source=*datasourse*;Initial Catalog=*incat*;User ID=*user*;Password=*pass*";
+                cnn = new SqlConnection(connetionString);
+                cnn.Open();
+                sql = "insert into *db_name* (name, surname, age, gender) values" + "(" +conc+ ")";
+                command = new SqlCommand(sql, cnn);
+                dataRead = command.ExecuteReader();
+                sql = "Select id,name,surname,age,gender";
+                command = new SqlCommand(sql, cnn);
+                dataRead = command.ExecuteReader();
+                while (dataRead.Read())
+                {
+                    Output = Output + dataRead.GetValue(0) + ", " + "\n";
+                }
+                MessageBox.Show(Output);
+                cnn.Close();
+            }
+        }
+
+        private void name_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
